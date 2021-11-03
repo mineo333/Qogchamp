7ea3e57ca8179c416c69a967a751f26587006f6e
sharad <sharadawsomeness@gmail.com>
2021-11-03 21:58:33 +0000

    Working e1000!

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/mineo333/GhostFops/Module.symvers := sed 's/\.ko$$/\.o/' /home/mineo333/GhostFops/modules.order | scripts/mod/modpost -m -a  -o /home/mineo333/GhostFops/Module.symvers -e -i Module.symvers   -T -
--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/mineo333/GhostFops/modules.order := {   echo /home/mineo333/GhostFops/qogchamp.ko; :; } | awk '!x[$$0]++' - > /home/mineo333/GhostFops/modules.order
